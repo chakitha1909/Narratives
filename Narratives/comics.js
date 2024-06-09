ddbbb2c968e25547ae02fb6cfc8e9ef2a8a4fc22
chakitha1909 <156594782+chakitha1909@users.noreply.github.com>
2024-06-09 19:18:51 +0530

    Delete Narratives directory

--- Narratives/comics.js
+++
@@ -1,9 +0,0 @@
-function toggleDropdown() {
-    const dropdownMenu = document.getElementById('dropdown-menu');
-    dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
-}
-
-function toggleFilter() {
-    const sidebar = document.querySelector('.sidebar');
-    sidebar.classList.toggle('active');
-}
